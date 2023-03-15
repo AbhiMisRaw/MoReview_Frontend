c1da51d5ec8eaabba52172a9b959870af513edb9
AbhiMisRaw <sumitmm9506555798@gmail.com>
2023-03-15 15:44:45 +0530

    build  user interface for Movie Application(MoReview)

---
+++ src/component/reviewForm/ReviewForm.js
@@ -0,0 +1,19 @@
+import { Form, Button } from "react-bootstrap";
+
+const ReviewForm = ( {handleSubmit , revText, LabelText, defaultValue} )=>{
+    return (
+        <Form>
+            <Form.Group  className="mb-3" controlId="exampleForm.controlTextarea1">
+                <Form.Label>
+                    {LabelText}
+                </Form.Label>
+                <Form.Control ref={revText} as="textarea" rows={3} defaultValue={defaultValue} />
+
+            </Form.Group>
+            <Button variant="outline-info" onClick={handleSubmit} >Submit</Button>
+        </Form>
+    )
+}
+
+
+export default ReviewForm
